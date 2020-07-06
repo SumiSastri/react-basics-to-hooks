fe79038ed3e8dec01a2a6b79b8a7dab7c4e00ff5
Sumi Sastri <Sumitra.Sastri@gvcgroup.com>
2020-07-07 00:23:21 +0100

    adds and displays search input

---
+++ breaking-bad/src/components/common/SearchInput.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SearchInput = () => {
+  return (
+    <div>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Search for your favourite character"
+      />
+    </div>
+  );
+};
+export default SearchInput;
--- breaking-bad/src/components/pages/characters/CharactersCatalog.js
+++ breaking-bad/src/components/pages/characters/CharactersCatalog.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import NavBar from "../../common/Navbar";
 import CharactersApi from "../../api-calls/CharactersApi";
+import SearchInput from "../../common/SearchInput";
 
 const CharactersCatalog = () => {
   return (
     <div>
       <NavBar />
+      <SearchInput />
       <CharactersApi />
     </div>
   );
