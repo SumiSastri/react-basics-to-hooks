c0a71f37cd9fbef3d1d1cb0f37e8e9179876fa7f
Sumi Sastri <sumisastri@cancer.org.uk>
2021-10-21 20:49:17 +0100

    Clean up videorama project add tests - move documentation to docs folder

--- progressive-web-apps/videorama/src/App.test.js
+++ progressive-web-apps/videorama/src/App.test.js
@@ -1,9 +1,39 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Arrange - the describe block
+describe("Videorama PWA app tests", () => {
+  const videoramaApp = <App />;
+
+  // test blocks
+  it("renders the logo image", async () => {
+    // query Nodes
+    const { getByAltText } = render(<App />);
+    const pageLogo = getByAltText("logo");
+    // Assertion
+    expect(pageLogo).toBeInTheDocument();
+    // Fail test
+    // expect(pageLogo).not.toBeInTheDocument();
+
+    // Use debugger to check elements and meta-data
+    screen.debug(pageLogo);
+  });
+
+  // unit tests one assertion per test block
+  it("renders the page heading", async () => {
+    const { getByText } = render(<App />);
+    const pageHeading = getByText(/Videorama/i);
+    expect(pageHeading).toBeInTheDocument();
+    // screen.debug(pageHeading);
+  });
+
+  it("renders the page title", async () => {
+    const { getByText } = render(<App />);
+    const pageTitle = getByText(/A Progressive Web App with React-Hooks/i);
+    expect(pageTitle).toContainHTML("h4");
+    // screen.debug(pageTitle);
+  });
+
+  // API calls
 });
